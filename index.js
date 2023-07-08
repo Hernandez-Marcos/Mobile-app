da328def2512eacd073a109805700785dd76fa99
SgtFreddy713 <captesxel@gmail.com>
2023-07-08 16:55:28 -0300

    ul element added
    Now there is an ul element that shows the items in the database.

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://realtime-database-2cef2-default-rtdb.firebaseio.com/",
-}
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value;
+
     push(shoppingListInDB, inputValue);
-    console.log(inputValue)
-})
+    
+    clearInputFieldEl();
+});
+
+onValue(shoppingListInDB, function(snapshot){
+    let itemsArray = Object.values(snapshot.val());
+    
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i]);
+    }
+});
+
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+};
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+};
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = "";
+}
